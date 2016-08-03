814dbe8ef28386417d4e511325daf3191be10ad2
Michael K <michael.kafarowski@gmail.com>
2016-08-03 12:47:28 -0400

    Basic Keyboard Detection
    Reorganized files and added some keyboard detection. No further updates
    for a week or so

--- src/bootld.asm
+++ src/bootld.asm
@@ -1,29 +1,50 @@
 ;Initially coded with MikeOS tutorial - planning to rewrite and continue building up
 	BITS 16
+	s_version db 'aliOS v0.0.1',0
+	s_newline db 0Ah,0Dh,0
+	s_prompt db '$ ',0
+	
 
 start:
 	mov ax, 07C0h	;Setting up a 4K stack space after bootloader
-	add ax, 288	;
+	add ax, 288
 	mov ss, ax
 	mov sp,4096
 
 	mov ax, 07C0h
 	mov ds, ax
 
-	mov si, text_string	;String position in memory goes into SI
+	mov si, s_version	;String position in memory goes into SI
 	call print_string	;Call the string method
+	
+	mov si, s_newline	
+	call print_string	
+		
+	mov si, s_prompt
+	call print_string
 
-	jmp $ 			;Once the string has been printed, jump infinite
+.key:
+	call wait_key
+	mov ah, 0Eh
+	int 10h
+	cmp al,0Dh
+	jne .key			
+	
+	mov si, s_newline	
+	call print_string		
+	mov si, s_prompt
+	call print_string
+	
+	jmp .key
+
+
 
 
 ;PRINT STRING METHOD
-text_string:
-	db 'aliOS -- Hello, World',0
 print_string:
 	mov ah, 0Eh     ;BIOS procedure - Calling int 10h with 0Eh in AH means output to screen
 
 .nextletter:
-	;call delay
 	lodsb			;Load a byte from the string at SI, increment SI Address
 	cmp al,0		;If zero, it's the end of the string
 	je .done		;Jump if equal to zero to .done
@@ -33,15 +54,11 @@ print_string:
 .done:
 	ret			;Returns from call location.
 
-delay:
-	;Yeah this kind of delay isnt going to work on a 2.5 GHz processor - use RTC instead?
-	push ax
-	mov ax,0
-.delay_repeat:
-	cmp ax,50000
-	inc ax
-	jne .delay_repeat
-	pop ax
+
+;WAIT FOR KEYBOARD METHOD
+wait_key:
+	mov ah, 0
+	int 16h
 	ret
 
 
